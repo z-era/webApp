2b94a15ad2d5f4ca3bd05d269a6e0250c0873044
z-era <972042562@qq.com>
2016-07-11 19:12:02 +0800

    setting environment0.1

---
+++ app/components/Favlist.vue
@@ -0,0 +1,19 @@
+<template>
+	<div v-for="n in 10">div</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		data() {
+			return {
+				msg:"Hello vue!"
+			}
+		}
+	}
+</script>
+
+<style>
+	html{
+		background: green;
+	}
+</style>
